2dd538c4d4f9e0a143993bd24cd3b113000ec776
zRosies <gustavo.enrollment@gmail.com>
2026-08-20 20:16:09 -0300

    Saving merges

--- apps/order-service/src/order-service.module.ts
+++ apps/order-service/src/order-service.module.ts
@@ -16,11 +16,7 @@ import { OrdersService } from './orders/orders.service';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-<<<<<<< HEAD
-      host: process.env.DB_HOST || 'localhost',
-=======
       host: process.env.DB_HOST ?? 'localhost',
->>>>>>> 7c9ff8f6e6cdb5423b9635465c9831fc643329c2
       port: parseInt(process.env.SERVER_PORT!),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
